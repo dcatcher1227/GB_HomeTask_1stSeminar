170806e85c2cc52daa807d0071a752e64f3f493c
DC <d.catcher2014@yandex.ru>
2022-07-21 14:28:23 +0300

    создал программу, которая на вход принимает два числа и выдаёт максимальное из этих чисел

---
+++ Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine ("Введите число a: ");
+int a = int.Parse( Console.ReadLine() );
+Console.WriteLine ("Введите число b: ");
+int b = int.Parse( Console.ReadLine() );
+if( a <= b )
+{
+    Console.WriteLine( b );
+}
+else
+{
+    Console.WriteLine( a );
+}
+
+
